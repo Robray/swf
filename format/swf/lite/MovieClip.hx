bd95a07c5e00f7c9a27d34c068d646f92c4db180
robert <robert.udalov@gmail.com>
2016-07-13 11:34:37 +0200

    gotoAndStop play until stopping frame

--- format/swf/lite/MovieClip.hx
+++ format/swf/lite/MovieClip.hx
@@ -157,16 +157,17 @@ class MovieClip extends flash.display.MovieClip {
 	
 	
 	public override function gotoAndPlay (frame:#if flash flash.utils.Object #else Dynamic #end, scene:String = null):Void {
-		
+
+		play ();
 		__currentFrame = __getFrame (frame);
 		__updateFrame ();
-		play ();
-		
+
 	}
 	
 	
 	public override function gotoAndStop (frame:#if flash flash.utils.Object #else Dynamic #end, scene:String = null):Void {
-		
+
+		play ();
 		__currentFrame = __getFrame (frame);
 		__updateFrame ();
 		stop ();
